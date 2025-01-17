9ab61d5a02963a21d5443b6917b055acb4e45f72
ganaa037 <tuvaangantumur@gmail.com>
2025-01-17 12:42:25 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,108 @@
+// for (let i = 1; i <= 5; i++) {
+//   let stars = " ";
+//   for (let k = 1; k <= i; k++) {
+//     stars += "*";
+//   }
+//   console.log(stars);
+// }
+// let stars = "";
+// for (let i = 5; i >= 1; i--) {
+//   let stars = "";
+//   for (let j = 0; j < i - 1; j++) {
+//     stars += "  ";
+//   }
+//   for (let k = 1; k <= 5 - i + 1; k++) {
+//     stars += "  * ";
+//   }
+//   console.log(stars);
+// }
+// const n = 5;
+// const c = 5;
+// for (let i = 1; i <= n; i++) {
+//   let stars = "";
+//   for (let k = 1; k <= n; k++) {
+//     if (i == 1 || i == n || k == 1 || k == c) {
+//       stars += " * ";
+//     } else if (i == 3 && k == 3) {
+//       stars += " * ";
+//     } else {
+//       stars += "   ";
+//     }
+//   }
+//   console.log(stars);
+// }
+
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i = i + 1) {
+//   if (i % 2 !== 0) {
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum);
+
+// let x = 121;
+// let reverse = "";
+// x = x.toString();
+// for (i = x.length - 1; i >= 0; i--) {
+//   reverse = reverse + x[i];
+// }
+// if (reverse == x) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// let num = ["1", "2", "3"];
+// let num2 = ["4", "5", "6"];
+
+// for (let i = 0; i < num2.length; i++) {
+//   num.push(num2[i]);
+// }
+
+// console.log(num);
+
+// let array = [2, 3, 4];
+// let array2 = [5, 6, 7];
+// let array3 = [];
+// for (let i = 0; i < array2.length; i++) {
+//   array3.push(array2[i] + array[i]);
+// }
+// console.log(array3);
+
+// const array = [2, 4, 7];
+// let sum = 0;
+// let max = 0;
+
+// for (let i = 1 ; i < array.length; i++) {
+//   console.log(array[i]);
+//   sum = array[i] + sum;
+//   if (max < array[i]) {
+//     max = array[i];
+//     console.log(max);
+//   }
+// }
+// console.log(sum);
+// console.log("max bol == ", max);
+// let reverse = [];
+// for (let j = array.length - 1; j >= 0; j--) {
+//   reverse.push(array[j]);
+// }
+// console.log(reverse);
+
+// let even = 0;
+// let odd = 0;
+
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] % 2 == 0) {
+//     even = even + 1;
+//   } else {
+//     odd = odd + 1;
+//   }
+// }
+// console.log(`even=>${even} odd=>${odd}`);
+
+// let array = [1, 2, 1];
+// for (let i = 0; i < array.length; i++) {
+//   for(let )
+// }
